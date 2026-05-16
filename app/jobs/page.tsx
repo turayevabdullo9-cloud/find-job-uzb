0c7f7566650e01d4370985d3662ada6f81aa8bef
Abdullo <turayevabdullo9@gmail.com>
2026-05-16 11:10:27 +0500

    third commit

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -135,9 +135,7 @@ const BrowseAllJobs = () => {
 
                             {filteredJobs.length === 0 && (
                                 <div className="rounded-3xl border border-slate-200 bg-white p-8 text-center text-slate-600">
-                                    git add .
-git commit -m "third commit"
-git push origin mainhech narsa topilmadi
+                                   hech narsa topilmadi
                                 </div>
                             )}
                         </div>
